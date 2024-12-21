9be4d38eba33b5749769800c2e59028e171ec807
Yunis Nabiyev <35284938+WAVForm@users.noreply.github.com>
2024-12-21 00:13:27 -0800

    player controller & dialogue
    still need to test interaction

--- dialogue_template.gd
+++ dialogue_template.gd
@@ -1,19 +1,110 @@
 extends Control
 
-@onready var visual = $split/visual
-@onready var text = $split/text_container
-@onready var text_background = $split/text_container/background
-@onready var text_content = $split/text_container/text
+@onready var visual:TextureRect = $split/visual
+@onready var text:Panel = $split/text_container
+@onready var text_content:RichTextLabel = $split/text_container/text
+@onready var doubleclick_timer:Timer = $doubleclick_timer
 
-var blank := Color(0,0,0,0)
-var start := false
+enum STATES {NONE, FADING_IN, DISPLAYING_TEXT, WAITING_FOR_CLICK, FADING_OUT, DOUBLE_CLICKED}
+
+var prev_state: STATES = STATES.NONE
+var current_state:STATES = STATES.NONE
+var current_time := 0.0
+var texts: Array[String]
+
+signal done
+
+@export var fade_time := 1.0
+@export var speedup_multiplier := 2.0
 
 func _ready():
-	visual.modulate = blank
-	text.modulate = blank
-	text_background.modulate = blank
+	visual.modulate.a = 0.0 #blank
+	text.modulate.a = 0.0 #blank
 	text_content.visible_ratio = 0
+	start(["first", "second", "third", "fourth"], PlaceholderTexture2D.new())
+
+func start(texts_p:Array[String], image_p:Texture2D):
+	texts = texts_p
+	visual.texture = image_p
+	current_state = STATES.FADING_IN
 
 func _process(delta):
-	if start:
-		
+	if current_state > STATES.NONE:
+		current_time += delta * (speedup_multiplier if Input.is_action_pressed("lmb_click") else 1) #update current time, with multiplier if LMB is being pressed
+
+		#check for double click
+		if Input.is_action_just_pressed("lmb_click"):
+			if doubleclick_timer.is_stopped():
+				doubleclick_timer.start()
+			else:
+				prev_state = current_state
+				current_state = STATES.DOUBLE_CLICKED
+
+		match current_state:
+			STATES.FADING_IN:
+				if(current_time > fade_time or is_equal_approx(current_time, fade_time)): #if done fading in
+					visual.modulate.a = 1.0 #make opaque
+					text.modulate.a = 1.0 #make opaque
+					current_time = 0.0 #reset time
+					text_content.text = texts.pop_front() #set first text
+					prev_state = STATES.FADING_IN
+					current_state = STATES.DISPLAYING_TEXT
+				else:
+					var amt = current_time / fade_time #fade amount = % of time left
+					visual.modulate.a = lerp(0.0, 1.0, amt) #fade
+					text.modulate.a = lerp(0.0, 1.0, amt) #fade
+			STATES.DISPLAYING_TEXT:
+				var amt = current_time / text_content.get_line_count() #amount of text shown, 1 second per line
+				if (amt > 1.0 or is_equal_approx(amt, 1.0)): #if all lines show
+					text_content.visible_ratio = 1.0 #make sure they show
+					current_time = 0.0 #reset time
+					prev_state = STATES.DISPLAYING_TEXT
+					current_state = STATES.WAITING_FOR_CLICK
+				else:
+					text_content.visible_ratio = amt #display correct amount of text
+			STATES.WAITING_FOR_CLICK:
+				if Input.is_action_pressed("lmb_click"): #if just clicked
+					current_time = 0.0 #reset time
+					prev_state = STATES.WAITING_FOR_CLICK
+					if texts.size() > 0: #if texts left to show
+							text_content.visible_ratio = 0.0 #reset amount of text shown
+							text_content.text = texts.pop_front() #set text to next
+							current_state = STATES.DISPLAYING_TEXT
+					else:
+							current_state = STATES.FADING_OUT
+			STATES.FADING_OUT:
+				if(current_time > fade_time or is_equal_approx(current_time, fade_time)): #if done fading out
+					visual.modulate.a = 0.0 #set to final value
+					text.modulate.a = 0.0 #seet to final value
+					current_time = 0.0 #reset time, not necessary
+					prev_state = STATES.FADING_OUT
+					current_state = STATES.NONE
+					done.emit() #let whatever needs to know that the dialogue is done
+				else:
+					var amt = current_time / fade_time #fade amount = % of tiem left
+					visual.modulate.a = lerp(1.0, 0.0, amt) #fade
+					text.modulate.a = lerp(1.0, 0.0, amt) #fade
+			STATES.DOUBLE_CLICKED:
+				match prev_state:
+					STATES.FADING_IN:
+						visual.modulate.a = 1.0 #make opaque
+						text.modulate.a = 1.0 #make opaque
+						current_time = 0.0 #reset time
+						text_content.text = texts.pop_front() #set first text
+						prev_state = STATES.FADING_IN
+						current_state = STATES.DISPLAYING_TEXT
+					STATES.DISPLAYING_TEXT:
+						prev_state = STATES.DISPLAYING_TEXT
+						if (texts.size() > 0):
+							text_content.visible_ratio = 1.0 #make opaque
+							current_time = 0.0 #reset time
+							current_state = STATES.WAITING_FOR_CLICK
+						else:
+							current_state = STATES.FADING_OUT
+					STATES.FADING_OUT:
+						visual.modulate.a = 0.0 #set to final value
+						text.modulate.a = 0.0 #seet to final value
+						current_time = 0.0 #reset time, not necessary
+						prev_state = STATES.FADING_OUT
+						current_state = STATES.NONE
+						done.emit() #let whatever needs to know that the dialogue is done
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,26 +3,56 @@ extends CharacterBody2D
 @export var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export_category("Movement")
 @export var max_speed := 300.0
-@export var accel := 5
+@export var min_speed := 50.0
+@export var accel := 15
+@export var decel := 20
 @export var jump_force := 500.0
+@export_category("Interaction")
+@export var interaction_range := 128
 
+@onready var ray = $interaction_ray
+
+enum STATES {IDLE, STOPPING, MOVING, JUMPING, FALLING, HIDING}
+var current_state:STATES = STATES.IDLE
 var speed := 0
+var dir := Vector2.RIGHT
+
+func _process(delta):
+	dir = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("look_down", "look_up")) #get direction for current frame
 
 func _physics_process(delta):
-	#gravity
-	velocity.y += gravity * delta
-	
 	try_move(delta)
+	try_interaction()
 	
 func try_move(delta):
-	var dir = Input.get_axis("move_left", "move_right")
-	if dir != 0:
-		if velocity.x < 0 and dir > 0 or velocity.x > 0 and dir < 0:
+	if is_on_floor() and dir.x == 0 and not Input.is_action_just_pressed("jump") and velocity.x == 0 and current_state != STATES.HIDING:
+		current_state = STATES.IDLE
+		return
+	
+	if dir.x != 0:
+		current_state = STATES.MOVING
+		if velocity.x < 0 and dir.x > 0 or velocity.x > 0 and dir.x < 0:
+			velocity.x *= -1
+		velocity.x += (dir.x*accel) if velocity.x < max_speed and velocity.x > -max_speed else 0
+	elif velocity.x != 0:
+		current_state = STATES.STOPPING
+		if velocity.x < 0 and velocity.x > -min_speed or velocity.x > 0 and velocity.x < min_speed:
 			velocity.x = 0
-		velocity.x += Input.get_axis("move_left", "move_right")*accel if (-max_speed < velocity.x and velocity.x < max_speed) else 0
-	else:
-		velocity.x = lerp(velocity.x, 0.0, 2*accel*delta)
-	velocity.y -= jump_force if Input.is_action_pressed("move_jump") and self.is_on_floor() else 0
+			current_state = STATES.IDLE
+		else:
+			velocity.x -= decel * (1 if velocity.x > 0 else -1)
 	
+	if not is_on_floor():
+		current_state = STATES.FALLING
+		velocity.y += gravity * delta #gravity
+	elif Input.is_action_just_pressed("jump"):
+		current_state = STATES.JUMPING
+		velocity.y -= jump_force
+
 	move_and_slide()
-	
+
+func try_interaction():
+	ray.target_position = dir*interaction_range
+	if Input.is_action_just_pressed("interact") and ray.is_colliding():
+		print("Y")
+		ray.get_collider().interact(self)
